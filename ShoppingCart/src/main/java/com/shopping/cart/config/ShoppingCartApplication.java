b86129025ba2a89a86a64d648543aab66fad95bb
VinodKumarKusuma <VinodKumarKusuma@DESKTOP-S84A1I1>
2017-11-23 23:16:29 +0530

    componentscan has been added.

--- ShoppingCart/src/main/java/com/shopping/cart/config/ShoppingCartApplication.java
+++ ShoppingCart/src/main/java/com/shopping/cart/config/ShoppingCartApplication.java
@@ -2,8 +2,10 @@ package com.shopping.cart.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com")
 public class ShoppingCartApplication {
 
 	public static void main(String[] args) {
